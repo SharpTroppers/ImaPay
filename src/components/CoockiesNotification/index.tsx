4fd4eef930e6fc61cc57ac9a1e668c1c6c5aba9d
Felipe Augusto L. C. Magalhães <felipealcmagalhaes@gmail.com>
2023-04-01 21:52:56 -0300

    feat: ajust modal components

--- src/components/CoockiesNotification/index.tsx
+++ src/components/CoockiesNotification/index.tsx
@@ -17,33 +17,47 @@ export function CoockiesNotification() {
   const handleClose = () => {
     setShowOpening(false);
   };
+
+  const handleReject = () => {
+    setShowOpening(false);
+  };
+
+  const handleAccept = () => {
+    setShowOpening(false);
+    // Add any logic for accepting the cookies here
+  };
+
   return (
     <>
       {showOpening && (
-        <div className={`${styles["coockies-modal"]}`}>
+        <div className={styles.coockiesModal}>
           <animated.div>
             <animated.div>
-              <animated.h3 className={`${styles["title-modal"]}`}>
+              <animated.h3 className={styles.titleModal}>
                 Valorizamos sua privacidade
               </animated.h3>
             </animated.div>
-            <animated.p className={`${styles["cookies"]}`}>
+            <animated.p className={styles.cookies}>
               Usamos cookies para aprimorar sua experiência de navegação,
               veicular anúncios ou conteúdo personalizado e analisar nosso
               tráfego. Ao clicar em "Aceitar", você concorda com o uso de
               cookies.
             </animated.p>
           </animated.div>
-          <animated.button
-            className={`${styles["cookies-btn"]} ${styles["reject-btn"]}`}
-          >
-            Rejeitar
-          </animated.button>
-          <animated.button
-            className={`${styles["cookies-btn"]} ${styles["accept-btn"]}`}
-          >
-            Aceitar
-          </animated.button>
+          <div className={styles.btnContainer}>
+            <button
+              className={`${styles.cookiesBtn} ${styles.rejectBtn}`}
+              onClick={handleReject}
+            >
+              Rejeitar
+            </button>
+            <button
+              className={`${styles.cookiesBtn} ${styles.acceptBtn}`}
+              onClick={handleAccept}
+            >
+              Aceitar
+            </button>
+          </div>
         </div>
       )}
     </>
