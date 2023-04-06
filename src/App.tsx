5b286eb127d44925048cea8bb12cbbd0980f58e7
Jonas Narvas Balbino <JNB.dev@outlook.com>
2023-04-05 23:58:13 -0300

    Adding transfer option and style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { UserPage } from "./pages/UserPage/User";
-import { Transfer } from "./pages/UserPage/Transfer";
+// import { Transfer } from "./pages/UserPage/Transfer";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { PasswordRecoverPage } from "./pages/PasswordRecoveryPage/PasswordRecoverPage";
 import { SignUpPage } from "./pages/SignUpPage/SignUpPage";
@@ -28,10 +28,6 @@ const router = createBrowserRouter([
   {
     path: "/user",
     element: <UserPage />,
-  },
-  {
-    path: "/transfer",
-    element: <Transfer />,
   },
   
   
