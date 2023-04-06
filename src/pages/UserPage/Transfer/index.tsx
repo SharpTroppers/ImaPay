1e2e5f51724db70414f387ce6998e3c990c47eb6
Daniel Carvalho <Danielscg2012@gmail.com>
2023-04-06 11:11:04 -0300

    implementação da user

--- src/pages/UserPage/Transfer/index.tsx
+++ src/pages/UserPage/Transfer/index.tsx
@@ -2,22 +2,18 @@ import React from "react";
 import styles from "./style.module.css";
 import { Balance } from "../balance";
 
-
-
-export function Transfer()  {
-
-
-    return(
-        
-        <form className = "form-transfer">
-        <label>Digite o número da conta ou documento do destinatário: </label>
-        <input type="number" id="account-num-or-doc-num"></input>
-        <label>Valor a ser transferido: </label>
-        <input type="number" id="currency-value" min="0.1" step={"any"}>R$ </input>
-        <label>Mensagem (Opcional): </label>
-        <input type="text" id="optional-message"></input>
-        <button type="button" ref="#"></button>
-        </form>
-    )
+export function Transfer() {
+  return (
+    <form className="form-transfer">
+      <label>Digite o número da conta ou documento do destinatário: </label>
+      <input type="number" id="account-num-or-doc-num"></input>
+      <label>Valor a ser transferido: </label>
+      <input type="number" id="currency-value" min="0.1" step={"any"}>
+        R${" "}
+      </input>
+      <label>Mensagem (Opcional): </label>
+      <input type="text" id="optional-message"></input>
+      <button type="button" ref="#"></button>
+    </form>
+  );
 }
-
