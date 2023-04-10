7f2f8e4002f94ce971fd47ee74e715415f4e8d64
gabriel-josantos <gabriel.josantos1@gmail.com>
2023-04-10 03:46:00 -0300

    merge signup

--- src/components/SignupPageSteps/SuccessfullSignup/index.tsx
+++ src/components/SignupPageSteps/SuccessfullSignup/index.tsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import styles from './styles.module.css'
-import { Link } from 'react-router-dom'
-import SuccessIcon from '../../../assets/img/tropper_logo.png'
+import React from "react";
+import styles from "./styles.module.css";
+import { NavLink } from "react-router-dom";
+import SuccessIcon from "../../../assets/img/trooper_logo.png";
 
 const SuccessfullSignup = () => {
   return (
-    <main className={styles["main-container-style"]} id={styles["main-container-success-sign-up-style"]}>
-        <header className={styles["forms-header-container"]} id={styles["success-welcome-message-style"]}>
-            <h1 id={styles["header-title"]}>Seja Bem-Vindo à Familia, Trooper</h1>
-        </header>
-        <img id={styles['success-icon']} src={SuccessIcon}/>
-        <Link id={styles["sign-up-go-to-login-page-style"]}  to='/'>
-            <button 
-                
-                className={styles["no-button-style"]}
-                >
-                    Login
-            </button>
-        </Link>
+    <main
+      className={styles["main-container-style"]}
+      id={styles["main-container-success-sign-up-style"]}
+    >
+      <header
+        className={styles["forms-header-container"]}
+        id={styles["success-welcome-message-style"]}
+      >
+        <h1 id={styles["header-title"]}>Seja Bem-Vindo à Familia, Trooper</h1>
+      </header>
+      <img id={styles["success-icon"]} src={SuccessIcon} />
+      <NavLink id={styles["sign-up-go-to-login-page-style"]} to='/'>
+        <button className={styles["no-button-style"]}>Login</button>
+      </NavLink>
     </main>
-)
-}
+  );
+};
 
-export default SuccessfullSignup
+export default SuccessfullSignup;
