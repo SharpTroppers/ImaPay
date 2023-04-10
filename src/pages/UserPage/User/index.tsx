7cd84f19b19ff4da1a2280eb5d13cebfb1755aa1
gabriel-josantos <gabriel.josantos1@gmail.com>
2023-04-10 19:49:51 -0300

    adicionado importação local para historico de transações, para deploy netlify

--- src/pages/UserPage/User/index.tsx
+++ src/pages/UserPage/User/index.tsx
@@ -1,36 +1,37 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import styles from "./style.module.css";
-import Money from "../../../assets/img/money-bold.svg";
+// import axios from "axios";
+// import styles from "./style.module.css";
+// import Money from "../../../assets/img/money-bold.svg";
 import { Balance } from "../Balance";
 import HeaderUserPage from "../HeaderUserPage";
 import Historic from "../Historic";
-import Transfer from "../Transfer";
+
+import transferData from "../../../helpers/transferData.json";
 
 export function UserPage() {
   const [userName, setUserName] = useState("Usuário");
-  const [userList, setUserList] = useState([]);
+  // const [userList, setUserList] = useState([]);
   const [balance, setBalance] = useState(50);
   const [balanceAvailable, setBalanceAvailable] = useState(25);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  // const openModal = () => {
+  //   setIsModalOpen(true);
+  // };
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:3333/users")
-      .then(({ data }) => setUserList(data));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:3333/users")
+  //     .then(({ data }) => setUserList(data));
+  // }, []);
 
   return (
     <div>
       <HeaderUserPage name={userName} />
       <Balance balance={balance} balanceAvailable={balanceAvailable} />
-      <Historic users={userList} />
+      <Historic users={transferData} />
     </div>
   );
 }
