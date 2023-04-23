565a1e653b3cb7d4fd94c027a760473cc83a8291
gabriel-josantos <gabriel.josantos1@gmail.com>
2023-04-23 19:47:17 -0300

    adicionado informações na homePage

--- ImaPay_FrontEnd/src/pages/UserPage/User/index.tsx
+++ ImaPay_FrontEnd/src/pages/UserPage/User/index.tsx
@@ -7,35 +7,58 @@ import HeaderUserPage from "../HeaderUserPage";
 import Historic from "../Historic";
 import { Modal } from "../MenuModal";
 import transferData from "../../../helpers/transferData.json";
+import axios from "axios";
+import jwt_Decode from "jwt-decode";
 
 export function UserPage() {
   const [userName, setUserName] = useState("Usuário");
   // const [userList, setUserList] = useState([]);
-  const [balance, setBalance] = useState(50);
+  const [balance, setBalance] = useState(0);
   const [balanceAvailable, setBalanceAvailable] = useState(25);
   const [open, setOpen] = useState(false);
   const handleClick = () => {
     setOpen(!open);
   };
 
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-  // const openModal = () => {
-  //   setIsModalOpen(true);
-  // };
-  // const closeModal = () => {
-  //   setIsModalOpen(false);
-  // };
+  const instance = axios.create({
+    baseURL: "https://localhost:7274",
+    headers: {
+      "Content-Type": "application",
+    },
+  });
 
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:3333/users")
-  //     .then(({ data }) => setUserList(data));
-  // }, []);
+  instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("Token")!;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+  // instance
+  //   .get("/endpoint")
+  //   .then((response) => {
+  //     console.log(response);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  const token = localStorage.getItem("Token")!;
+
+  const payload = jwt_Decode(token) as { Id: string };
+  useEffect(() => {
+    instance
+      .get(`/users/${payload.Id}`)
+      .then((response) => {
+        setUserName(response.data.userProfile.name);
+        setBalance(response.data.account.balance);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div>
       <HeaderUserPage name={userName} />
-      <Balance balance={balance} balanceAvailable={balanceAvailable} />
+      <Balance balance={balance} balanceAvailable={balance} />
       <Historic users={transferData} />
     </div>
   );
