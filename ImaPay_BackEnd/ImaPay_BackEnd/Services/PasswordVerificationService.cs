235c2d581843075dea0daa24a6bd257b02f9b25c
allan-gilber <allangilber@yahoo.com.br>
2023-04-23 16:14:14 -0300

    feat: criado verificador de hashs do password.

---
+++ ImaPay_BackEnd/ImaPay_BackEnd/Services/PasswordVerificationService.cs
@@ -0,0 +1,16 @@
+﻿namespace ImaPay_BackEnd.Services
+{
+    public class PasswordVerificationService
+    {
+        public bool CheckPassword(string hashedPassword, string dbHashedPassword)
+        {
+            bool isPasswordCorrect = BCrypt.Net.BCrypt.Verify(hashedPassword, dbHashedPassword);
+            return isPasswordCorrect;
+        }
+
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+    }
+}
